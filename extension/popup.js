fb78a37786d3a4205b1993e135b08b0172a9cff3
jwhitekim <jwhitekim@gmail.com>
2026-04-19 22:51:42 +0900

    Edit

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const SERVER = 'http://localhost:8000';
+const SERVER = 'https://simmering-harbor-common.ngrok-free.dev';
 
 const dropzone  = document.getElementById('dropzone');
 const fileInput = document.getElementById('fileInput');
